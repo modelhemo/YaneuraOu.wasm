a589de14d293f04cbb95ef4626f498a7bc4b6163
modelhemo <o0o0nvn0o0o@icloud.com>
2024-12-10 18:03:11 +0900

    first_commit

---
+++ yaneuraou.halfkp.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="emscripten" />
+import { YaneuraOuModule } from "./yaneuraou.module";
+
+declare const YaneuraOu_HalfKP: EmscriptenModuleFactory<YaneuraOuModule>;
+export = YaneuraOu_HalfKP;
