2ba5911553bc4add62221ddb4ca99f363e6d48cd
modelhemo <o0o0nvn0o0o@icloud.com>
2024-12-10 18:45:53 +0900

    add-sw

---
+++ coi-serviceworker.js
@@ -0,0 +1,21 @@
+event.respondWith(
+            fetch(request)
+                .then((response) => {
+                    if (response.status === 0) {
+                        return response;
+                    }
+
+                    const newHeaders = new Headers(response.headers);
+                    newHeaders.set("Cross-Origin-Embedder-Policy",
+                        coepCredentialless ? "credentialless" : "require-corp"
+                    );
+                    newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                    return new Response(response.body, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: newHeaders,
+                    });
+                })
+                .catch((e) => console.error(e))
+        );
